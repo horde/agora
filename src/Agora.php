bc365ab1127b59a4b17a8462fd77be8b211881c2
Ralf Lang <ralf.lang@ralf-lang.de>
2026-03-13 08:29:06 +0100

    feat: Upgrade agora to composer native for Horde 6
    Rename CLI script and port from PEAR to Horde ARGV library
    Dependency updates.

---
+++ src/Agora.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright 2005-2026 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (GPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/gpl.
+ *
+ * @category Horde
+ * @package  Agora
+ * @author   Ralf Lang <lang@b1-systems.de>
+ * @license  http://www.horde.org/licenses/gpl GPL
+ */
+
+namespace Horde\Agora;
+
+/**
+ * Agora application API.
+ *
+ * This file provides a placeholder for the modern PSR-4 implementation
+ * of the Agora application.
+ *
+ * @category Horde
+ * @package  Agora
+ * @author   Ralf Lang <lang@b1-systems.de>
+ * @license  http://www.horde.org/licenses/gpl GPL
+ */
+class Agora
+{
+}
